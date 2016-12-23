e0a9a9f103b96f0d2842db3a5643f97f896bc07f
Andrey Mashukov <a.mashukov@yandex.ru>
2016-12-23 17:21:01 +0800

    Add tests for count, next, sendTo

--- tests/ConveyorTest.php
+++ tests/ConveyorTest.php
@@ -62,7 +62,7 @@ class ConveyorTest extends PHPUnit_Framework_TestCase
 	public function testShouldAllowToAddGetAndRemoveElements()
 	    {
 		$conveyor = new Conveyor(__DIR__ . "/AdSchema.xsd");
-		$doc = new DOMDocument("1.0", "utf-8");
+		$doc      = new DOMDocument("1.0", "utf-8");
 		$doc->load(__DIR__ . "/example.xml");
 		$time = time();
 		$conveyor->add($doc, "sender");
@@ -74,6 +74,74 @@ class ConveyorTest extends PHPUnit_Framework_TestCase
 		$elements = $conveyor->getAllBySender("sender");
 		$this->assertEquals(array(), $elements);
 	    } //end testShouldAllowToAddGetAndRemoveElements()
+
+
+	/**
+	 * Should return count of elements in conveyor by sender
+	 *
+	 * @return void
+	 */
+
+	public function testShouldReturnCountOfElementsInConveyorBySender()
+	    {
+		$conveyor = new Conveyor(__DIR__ . "/AdSchema.xsd");
+		$doc      = new DOMDocument("1.0", "utf-8");
+		$doc->load(__DIR__ . "/example.xml");
+		$n = 100;
+		for ($i = 0; $i < $n; $i++)
+		    {
+			$conveyor->add($doc, "sender");
+		    }
+		$this->assertEquals($n, $conveyor->count("sender"));
+	    } //end testShouldReturnCountOfElementsInConveyorBySender()
+
+
+	/**
+	 * Should move element to next service (sender)
+	 *
+	 * @return void
+	 */
+
+	public function testShouldMoveElementToNextService()
+	    {
+		$conveyor = new Conveyor(__DIR__ . "/AdSchema.xsd");
+		$doc      = new DOMDocument("1.0", "utf-8");
+		$doc->load(__DIR__ . "/example.xml");
+		$n = 10;
+		for ($i = 0; $i < $n; $i++)
+		    {
+			$conveyor->add($doc, "sender");
+		    }
+		$elements = $conveyor->getAllBySender("sender");
+		foreach ($elements as $element)
+		    {
+			$conveyor->next($element, "nextservice");
+		    }
+		$this->assertEquals($n, $conveyor->count("nextservice"));
+	    } //end testShouldMoveElementToNextService()
+
+
+	/**
+	 * Should allow to move any count of elements to another sender
+	 *
+	 * @return void
+	 */
+
+	public function testShouldAllowToMoveAnyCountOfElementsToAnotherSender()
+	    {
+		$conveyor = new Conveyor(__DIR__ . "/AdSchema.xsd");
+		$doc      = new DOMDocument("1.0", "utf-8");
+		$doc->load(__DIR__ . "/example.xml");
+		$n = 10;
+		for ($i = 0; $i < $n; $i++)
+		    {
+			$conveyor->add($doc, "sender");
+		    }
+		$elements = $conveyor->getAllBySender("sender");
+		$conveyor->sendTo("sender", "nextservice", 5);
+		$this->assertEquals(5, $conveyor->count("nextservice"));
+		$this->assertEquals(5, $conveyor->count("sender"));
+	    } //end testShouldAllowToMoveAnyCountOfElementsToAnotherSender()
 
 
     } //end class
